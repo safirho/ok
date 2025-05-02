c36c6d2c0effdbfe850b7adcff207d93aad56698
safirho <omar.ramirez@anam.gob.mx>
2025-05-01 23:20:14 -0600

    Add files via upload

---
+++ principal/views.py
@@ -0,0 +1,14 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("""
+        <h1>¡Hola desde Azure + Django!</h1>
+        <p>Power BI embebido (ejemplo):</p>
+        <iframe width='800' height='600'
+                src='https://app.powerbi.com/reportEmbed?reportId=3705c713-72ec-428f-878a-0ec8ed9efdf7&autoAuth=true&ctid=2ce69147-2638-4f36-8769-4eb269cb1526'
+                frameborder='0' allowFullScreen='true'>
+        </iframe>
+    """)
