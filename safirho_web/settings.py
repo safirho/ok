6f81be9fd1cb8ad14285e4163221c847ca97e321
safirho <omar.ramirez@anam.gob.mx>
2025-05-02 00:26:32 -0600

    Add files via upload

--- safirho_web/settings.py
+++ safirho_web/settings.py
@@ -121,3 +121,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://safirho.mx',
+    'https://www.safirho.mx',  # si usas el subdominio también
+]
+
+
